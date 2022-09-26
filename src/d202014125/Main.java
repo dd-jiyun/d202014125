c2e7fe189c907f1c89e368b58ca88139d821cbb2
jiyun <dlwldbs8015@gmail.com>
2022-09-26 15:18:40 +0900

    Main.java 수정 #2

--- src/d202014125/Main.java
+++ src/d202014125/Main.java
@@ -2,8 +2,12 @@ package d202014125;
 
 
 public class Main {
+
+	static void say(String s) { 
+		System.out.println(s); 
+		}
+
 	public static void main(String[] args) {
-		String s = "hello world";
-		System.out.println(s);
+		say("hello world");
 	}
 }
